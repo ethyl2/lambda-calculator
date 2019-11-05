d45692b97397a68a15e6ce15ae7c0923d8cec4cd
Heather Nuffer <zzzuzz@gmail.com>
2019-11-05 16:10:41 -0700

    Added styling to operator and special buttons

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -4,7 +4,7 @@ const OperatorButton = (props) => {
   const { operator } = props;
   console.log(props);
   return (
-    <button>
+    <button className="button operatorButton">
       {operator}
       {/* Display a button element rendering the data being
        passed down from the parent container on props */}
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -11,13 +11,13 @@ const Operators = () => {
   const [operatorState, setOperatorState] = useState(operators);
   // STEP 2 - add the imported data to state
   return (
-    <div>
+    <div className="operatorsContainer">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
        {operatorState.map(operator => {
          console.log(operator)
-         return <OperatorButton operator={operator.value} key={operator.value} />
+         return <OperatorButton operator={operator.char} key={operator.value} />
        })}
       
     </div>
